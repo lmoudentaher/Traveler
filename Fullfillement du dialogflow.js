cd1b58583c3254ec33da5cc91670f0bbd3b1f783
Taher Lmouden <126186348+lmoudentaher@users.noreply.github.com>
2023-03-16 21:47:47 +0100

    Update Fullfillement du dialogflow.js

--- Fullfillement du dialogflow.js
+++ Fullfillement du dialogflow.js
@@ -1,5 +1,4 @@
-// See https://github.com/dialogflow/dialogflow-fulfillment-nodejs
-// for Dialogflow fulfillment library docs, samples, and to report issues
+
 'use strict';
  
 const functions = require('firebase-functions');
